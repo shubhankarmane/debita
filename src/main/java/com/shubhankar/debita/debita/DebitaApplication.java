092993d4398e6cb87789f752e95e374472a1189c
Shubhankar Mane <maneshubhankar@outlook.com>
2021-11-26 12:27:23 +0530

    Initial commit.

---
+++ src/main/java/com/shubhankar/debita/debita/DebitaApplication.java
@@ -0,0 +1,13 @@
+package com.shubhankar.debita.debita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhankar/debita/debita/DebitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhankar.debita.debita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
